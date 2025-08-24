c64c1cb2d20c341cc37035a5b824c5f55c2922e8
Esteban Ramirez <estebanraul313@gmail.com>
2025-08-24 11:16:49 -0500

    fix

--- src/app/views/register/register.module.ts
+++ src/app/views/register/register.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'primeng/api';
+import { RegisterComponent } from './register.component';
+import { RegisterRoutes } from './register.routes';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  imports: [CommonModule, SharedModule, RegisterRoutes],
+  declarations: [RegisterComponent],
 })
 export class RegisterModule {}
