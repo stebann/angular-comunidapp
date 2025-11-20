65ec76f7e247f1c62a9dd1d8556d98eea93619d8
Esteban Ramirez <estebanraul313@gmail.com>
2025-11-20 18:39:27 -0500

    mis negocios

--- src/app/views/menu/components/comercios/mis-negocios/mis-negocios.component.ts
+++ src/app/views/menu/components/comercios/mis-negocios/mis-negocios.component.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
+import { FilterOption } from 'src/app/shared/models/filter-models';
+import { FiltersService } from 'src/app/shared/services/filters.service';
+import { ComercioService } from '../services/comercio.service';
 
 @Component({
   selector: 'app-mis-negocios',
   templateUrl: './mis-negocios.component.html',
   styleUrls: ['./mis-negocios.component.scss'],
 })
-export class MisNegociosComponent { }
+export class MisNegociosComponent {
+  searchTerm: string = '';
+  soloFavoritos: boolean = false;
+  favoritos: number[] = [];
+  isOpen: boolean = false;
+
+  categorias: FilterOption[] = [];
+  condiciones: FilterOption[] = [];
+  tiposTransaccion: FilterOption[] = [];
+
+  constructor(
+    public comercioService: ComercioService,
+    private filterService: FiltersService
+  ) {}
+
+  ngOnInit(): void {
+    this.filterService
+      .getCategorias()
+      .subscribe((categorias) => (this.categorias = categorias));
+
+    this.filterService
+      .getCondiciones()
+      .subscribe((condiciones) => (this.condiciones = condiciones));
+
+    this.filterService
+      .getTiposTransaccion()
+      .subscribe((tipos) => (this.tiposTransaccion = tipos));
+  }
+
+  get filtro() {
+    return this.comercioService.filtroComercio;
+  }
+
+  onFiltersApplied(): void {}
+
+  openFilters() {
+    this.isOpen = true;
+  }
+}
